466e8c4013a1753c5cc0797bbf0de2179498bfb1
Jason Doe <jasondoe@arizona.edu>
2024-03-31 20:54:46 -0700

    Finished code

--- clean.py
+++ clean.py
@@ -1,9 +1,7 @@
 import openpyxl
 import config
 
-
 filePath = config.FILE_PATH
-dataRange = config.DATA_RANGE
 timeLimit = config.TIME_LIMIT
 
 try:
@@ -17,19 +15,43 @@ try:
     newClean = spreadsheet.copy_worksheet(spreadsheet["original"])
     newClean.title = "new clean"
 
-    index = 3
+    rowNumber = 3
+    deleteIndex = 0
+    maxRow = 0
+    deleteList = []
 
-    print(newClean[config.DATA_RANGE])
+    print("Starting cleanup")
 
-    for row in newClean.iter_rows(min_row = index, values_only=True):
-        dataDuration = newClean[config.DURATION_COL + str(index)].value
+    for row in newClean.iter_rows(min_row = rowNumber, values_only=True):
+        dataDuration = newClean[config.DURATION_COL + str(rowNumber)].value
 
         if (int(dataDuration) < timeLimit):
-            print("Time is less than " + str(timeLimit))
-        else:
-            print("Acceptable Time!")
-        index = index + 1
+            deleteList.append(rowNumber)
+        
 
+        dataColumns = newClean[config.DATA_RANGE_LOWER + str(rowNumber) + ":" + config.DATA_RANGE_UPPER + str(rowNumber)]
+
+        for row in dataColumns:
+            for cell in row:
+                if cell.value == None:
+                    if rowNumber not in deleteList:
+                        deleteList.append(rowNumber)
+                    break
+                
+
+        rowNumber = rowNumber + 1
+
+        maxRow = rowNumber
+    
+    
+    for num in deleteList:
+        newClean.delete_rows(num - deleteIndex)
+        deleteIndex = deleteIndex + 1
+        print("Deleting row " + str(num))
+
+    print("There are " + str(maxRow) + " currently. " + str(len(deleteList)) + " rows will be removed. New row count is " + str(maxRow - len(deleteList)) + ".")
+    print("Complete!")
+    
     spreadsheet.save(filePath)
 
 
