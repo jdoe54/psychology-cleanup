6135ff9d775d47ec857e8e136262b60bdd432e41
Jason Doe <jasondoe@arizona.edu>
2024-03-31 20:11:16 -0700

    Add code to find acceptable time

--- clean.py
+++ clean.py
@@ -4,6 +4,7 @@ import config
 
 filePath = config.FILE_PATH
 dataRange = config.DATA_RANGE
+timeLimit = config.TIME_LIMIT
 
 try:
 
@@ -13,12 +14,21 @@ try:
     original = spreadsheet["original"]
     clean = spreadsheet["clean"]
 
-    newSheet = spreadsheet.copy_worksheet(spreadsheet["original"])
-    newSheet.title = "Copy"
+    newClean = spreadsheet.copy_worksheet(spreadsheet["original"])
+    newClean.title = "new clean"
 
+    index = 3
 
-    #for row in original.iter_rows(min_row=2, values_only=True):
-    #    print(row["P"])
+    print(newClean[config.DATA_RANGE])
+
+    for row in newClean.iter_rows(min_row = index, values_only=True):
+        dataDuration = newClean[config.DURATION_COL + str(index)].value
+
+        if (int(dataDuration) < timeLimit):
+            print("Time is less than " + str(timeLimit))
+        else:
+            print("Acceptable Time!")
+        index = index + 1
 
     spreadsheet.save(filePath)
 
