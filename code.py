cf539d1dc0d2b3850d342f2ca8064f770523232d
Jason Doe <jasondoe@arizona.edu>
2024-03-31 19:34:45 -0700

    Added config file

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
 import openpyxl
+import config
 
 
-filePath = ""
+filePath = config.FILE_PATH
 
 try:
 
     spreadsheet = openpyxl.load_workbook(filePath)
     print("File found")
+
+    original = spreadsheet["original"]
+    clean = spreadsheet["clean"]
+
+    for row in original.iter_rows(min_row=2, values_only=True):
+        
 
 
 
